bb889e1dc53562cd3fedff86dcbe291ad63aae20
Achthar <69119535+Achthar@users.noreply.github.com>
2026-01-02 20:01:23 +0100

    change endpoint

--- src/components/lending/swap/Execute.tsx
+++ src/components/lending/swap/Execute.tsx
@@ -9,7 +9,7 @@ type Props = {
 }
 
 const getEndpoint = (swapType: 'debt' | 'collateral') => {
-  const baseUrl = 'http://127.0.0.1:8787'
+  const baseUrl = 'https://beta.api.1delta.io'
   return swapType === 'debt' ? `${baseUrl}/debt-swap` : `${baseUrl}/collateral-swap`
 }
 
